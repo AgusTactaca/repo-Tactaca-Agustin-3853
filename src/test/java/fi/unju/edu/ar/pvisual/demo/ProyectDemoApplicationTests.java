69e7a0647bdbe0b5fcdac842f2bd8f977b65a72b
Agustin <aguu73470@gmail.com>
2022-04-18 15:17:50 -0300

    primer commit

---
+++ src/test/java/fi/unju/edu/ar/pvisual/demo/ProyectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.pvisual.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
